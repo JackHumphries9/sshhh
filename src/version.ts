fd02a75e001abae0c9fed4a47860caa876086fdd
Jack Humphries <jackphumphries02@gmail.com>
2023-01-03 15:16:37 +0000

    Version bump

--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const PKG_VERSION = "1.0.2";export const PKG_DESCRIPTION = "A command line tool to generate random secret keys";
+export const PKG_VERSION = "1.0.3";export const PKG_DESCRIPTION = "A command line tool to generate random secret keys";
